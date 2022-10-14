36de0ae1bd57707ec19fe8661b2697f0fdca988a
M. Sarhan Akasah <114375641+SarhanAkasah@users.noreply.github.com>
2022-10-14 20:35:12 +0700

    Add files via upload

---
+++ Handphone.java
@@ -0,0 +1,31 @@
+//M.Sarhan akasah
+//20210040210
+//TI21G
+
+import java.util.Set;
+
+public class Handphone {
+     static String jenis_hp;
+    static int tahun_pembuatan;
+
+     public void setDataHP (String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+    public String getJenis_hp() {
+        return jenis_hp;
+    }
+    public int getTahun_pembuatan() {
+        return tahun_pembuatan;
+    }
+    public static void main(String[] args) {
+        Handphone hp = Handphone();
+        hp.setDataHP(jenis_hp, tahun_pembuatan);
+        hp.getJenis_hp();
+        hp.getTahun_pembuatan();
+    }
+    private static Handphone Handphone() {
+        return null;
+    }
+
+}
---
+++ sesi2.java
@@ -0,0 +1,26 @@
+//M.Sarhan akasah
+//20210040210
+//TI21G
+
+1. Jelaskan apa yang di maksud dengan class dan object!
+jawab : 
+Object-Oriented Programming (OOP) merupakan paradigma pemrograman yang menjadikan suatu objek sebagai fokus dalam mengembangkan aplikasi.
+Class merupakan blueprint / rancangan dari suatu objek. Class adalah gambaran umum dari suatu objek.
+
+2. Sebutkan dan jelaskan jenis-jenis method
+jawab :
+a. Konstruktur
+Method yang dijalankan pada awal permulaan jalannya program.
+b. Mutator
+Digunakan object untuk mengubah data baik itu berupa variabel instan ataupun static.
+
+3. Berdasarkan gambar berikut ini, Jelaskan masing-masing bagian sesuai dengan nomor yang ada!
+jawab :
+1.Nama Class
+2.Atribut
+3.Method Set
+4.Method Get untuk jenis
+5.Method Get untuk Merk
+6.Syntax untuk memanggil class
+7.Method set menginput
+8.Syntax untuk  meng print
